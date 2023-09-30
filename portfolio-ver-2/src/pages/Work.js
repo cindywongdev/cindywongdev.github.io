0c37bc9882d83421b45fe5e7b2ee4c25245a5699
Cindy Wong <cindywongdev@gmail.com>
2023-09-29 21:32:30 -0400

    transferred Work aka projects page

---
+++ portfolio-ver-2/src/pages/Work.js
@@ -0,0 +1,38 @@
+const Projects = (props) => {
+    const projects = require('../data/projects.json')
+
+    const imageStyling = {
+        display: "block",
+        width: "30%",
+        minWidth: "400px"
+    }
+
+    return (<><main>
+        <a id="work"></a>
+        <h1 class="section-heading text-center">Work</h1>
+        {projects.map((project) => (
+            <div class="project-card d-flex-col ai-center">
+                    <h1>{project.name}</h1>
+
+                    <p class="text">{project.description}</p>
+
+                    <a href={project.live}>
+                    <img src={project.image} alt={project.name} style={imageStyling}/>
+                    </a>
+
+                    <p class="text">{project.technologies}</p>
+
+                    <container class="buttons-container">
+                        <a href={project.live} target="_blank" rel="noreferrer">
+                            <button> Live Demo</button>
+                        </a>
+                        <a href={project.git} target="_blank" rel="noreferrer">
+                            <button>Github Repo</button>
+                        </a>
+                    </container>
+            </div>
+        ))}
+    </main></>)
+}
+
+export default Projects
